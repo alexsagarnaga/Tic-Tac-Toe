6ad336e8429439f8fa933b593a9d55b1ef732fe5
Alejandro <alexsagarnaga@gmail.com>
2022-03-02 13:20:10 -0600

    Added Header - Board grid for tic tac toe  - Game Mode Players Box

---
+++ index.js
@@ -0,0 +1,56 @@
+// control of game what I want my game to essentially do
+
+let gameState = {
+threeInARow: "You Win!",
+noneInARow: "Tie!",
+currentScore: 0,
+goalScore: 5,  
+
+}
+
+let nameDisplay = {
+    Player1: "Player One",
+    Player2: "Player Two",
+    playerComputer: "Computer"
+} 
+
+
+
+
+function scoreIncrease () {
+    gameState.currentScore++
+    scoreIncrease ()
+
+}
+
+
+console.log ({gameState})
+
+
+
+
+
+
+function togglePlayer () {
+    nameDisplay.Player1
+} 
+
+
+
+
+
+
+
+// functions that I will need to help my game do items expressed above
+
+
+
+
+
+
+
+
+
+
+
+// getting my control items and functions to interact together
