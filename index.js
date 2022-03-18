07803648e4e6c96099fe07596ba22574c0b85638
Alejandro <alexsagarnaga@gmail.com>
2022-03-18 13:37:46 -0500

    Added place holder with instructions into name input boxes so user can know to insert name and then press button below to display name, corrected x and o token to not change within cell, used spread operator in my restartgame function to properly restart game

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 // control of game what I want my game to essentially do
 
 const gameState = {
-players: ['x', 'o'],
+  players: ["x", "o"],
+};
 
-}
+let currentPlayer = "x";
 
-let currentPlayer = 'x'
+const board = document.querySelector(".board");
+board.addEventListener("click", function (event) {
+  console.log(event);
 
-const board = document.querySelector('.board')
-board.addEventListener('click', function (event) {
-    console.log(event)
-    // .target.id
-    event.target.innerHTML= currentPlayer
-    if (currentPlayer === 'x') {
-        currentPlayer = 'o' 
-    } else if (currentPlayer === 'o') {
-        currentPlayer = 'x'  
-        }
-})
-
-
+  if (currentPlayer === "x" && event.target.innerHTML === "") {
+    event.target.innerHTML = currentPlayer;
+    currentPlayer = "o";
+  } else if (currentPlayer === "o" && event.target.innerHTML === "") {
+    event.target.innerHTML = currentPlayer;
+    currentPlayer = "x";
+  }
+});
 
 // / functions that I will need to help my game do items expressed above
 function myFunction() {
-    let x = document.getElementById("myText").value;
-    document.getElementById("demo").innerHTML = x;
-  }
-    myFunction ()
+  let x = document.getElementById("p1Input").value;
+  document.getElementById("demo").innerHTML =
+    x + " : " + " You Are Mighty Token 'X'";
+}
+myFunction();
 
 function myFunctionTwo() {
-    let x = document.getElementById("myTextTwo").value;
-    document.getElementById("demoTwo").innerHTML = x; 
- }
-        myFunctionTwo ()
-
-function restartFunction () {
-    location.reload();
+  let o = document.getElementById("p2Input").value;
+  document.getElementById("demoTwo").innerHTML =
+    o + " : " + " You Are Mighty Token 'O'";
 }
+myFunctionTwo();
 
+function restartFunction() {
+  let grid = [...document.querySelectorAll(".cell")];
 
+  grid.forEach((item) => (item.innerHTML = ""));
+}
